369a6ed16af7e4b4c5af543b07b6ca77c599656c
Javie Jade Eresmas <eresmasjaviejade@gmail.com>
2026-09-07 20:41:18 +0800

    initial commit

---
+++ day1.js
@@ -0,0 +1,14 @@
+let name = "Javie Jade Eresmas";
+let age = 23;
+let bday = "July 10, 2003";
+let address = "Paglaum Village Valencia City, Bukidnon";
+let school = "Bukidnon State University";
+let isStudent = true;
+let favgame  = "Honor of Kings";
+let favsport = "Football";
+let favpersons = ["Parents", "Shy", "Lyza", "Church Friends"];
+
+console.log("My Name is " + name + " I'm " + 23 + " year old my birthday was on " + bday + " and I lived in " + address + " and I'm Studying in " + school + " my favorite video game is " + favgame + " my favorite sport is " + favsport + " my favorite persons are my " + favpersons );
+
+
+  
