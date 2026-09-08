8e072dfc554632d75124de88a7ed021010fcfc97
Javie Jade Eresmas <eresmasjaviejade@gmail.com>
2026-09-08 19:49:21 +0800

    done with variables

--- day1.js
+++ day1.js
@@ -1,14 +1,39 @@
-let name = "Javie Jade Eresmas";
-let age = 23;
-let bday = "July 10, 2003";
-let address = "Paglaum Village Valencia City, Bukidnon";
-let school = "Bukidnon State University";
-let isStudent = true;
-let favgame  = "Honor of Kings";
-let favsport = "Football";
-let favpersons = ["Parents", "Shy", "Lyza", "Church Friends"];
 
-console.log("My Name is " + name + " I'm " + 23 + " year old my birthday was on " + bday + " and I lived in " + address + " and I'm Studying in " + school + " my favorite video game is " + favgame + " my favorite sport is " + favsport + " my favorite persons are my " + favpersons );
+// THIS PART IS ALL ABOUT let,const,Variables,Strings,Numbers,console.log()
 
+// const name = "Javie Jade Eresmas";
+// const age = 23;
+// const course = "BSIT";
+// const university = "Bukidnon State University";
+// let favoritegame = "Honor of Kings";
 
-  
+// favoritegame = "PUBG";
+
+// console.log(name);
+// console.log(age);
+// console.log(course);
+// console.log(university);
+// console.log(favoritegame);
+
+// const grade = "A";
+// let Score = 100;
+// console.log("Grade: " + grade);
+// console.log("Score: " + Score);
+
+// const name = "Javie Jade Eresmas";
+// let age = 23;
+// console.log("Hi, my name is " + name + " and I am "+ age + " years old");
+
+// const birthYear = 2005; // this never changes
+// let currentAge = 20;    // this can update
+
+// currentAge = 21;        // valid
+
+// console.log(currentAge);
+// console.log(birthYear); // try this — it should throw an error!
+
+// let item = "Keyboard";
+// let price = 500;
+// let quantity = 2
+
+// console.log("Item: "+ item + "Price: " + price * quantity);
